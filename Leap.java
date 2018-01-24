47865d94b417eaf6703c1e4a4f51871ad5d5af19
teterkin <teterkin@gmail.com>
2018-01-24 23:48:20 +0500

    Leap done.

--- Leap.java
+++ Leap.java
@@ -1,4 +1,36 @@
 public class Leap
 {
-    
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Please use an year to check.");
+            usage();
+        }
+        else
+        {
+            try
+            {
+                int year = Integer.parseInt(args[0]);
+                if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+                {
+                    System.out.println("leap year!");
+                }
+                else
+                {
+                    System.out.println("not a leap year!");
+                }
+            }
+            catch (NumberFormatException ex)
+            {
+                System.out.println("Argument should be a number.");
+                usage();
+            }
+        }
+    }
+
+    private static void usage()
+    {
+        System.out.println("Usage: java Leap 2000");
+    }
 }
